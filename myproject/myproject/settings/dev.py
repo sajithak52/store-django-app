134212ddc1aaf45e48a1767eaca3802ba39565d1
Praveen <praveenpspravee@gmail.com>
2022-06-07 14:24:31 +0530

    new fix

--- myproject/myproject/settings/dev.py
+++ myproject/myproject/settings/dev.py
@@ -6,3 +6,15 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = 'rv4971254@gmail.com'
+# EMAIL_HOST_PASSWORD = '9656582215'
+EMAIL_HOST_USER = 'mission242022@gmail.com'
+EMAIL_HOST_PASSWORD = 'tuhzllskvtlavuyv'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+
+
